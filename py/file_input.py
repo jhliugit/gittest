bffaa2b348e40cbd2e21cccbcf40ae1da068ccfe
jhliugit <13660313156@139.com>
2019-11-03 14:43:48 +0800

    file

---
+++ py/file_input.py
@@ -0,0 +1,5 @@
+import fileinput
+for line in fileinput.input():
+    meta = [ fileinput.filename(), fileinput.fileno(), fileinput.filelineno(), fileinput.isfirstline(), fileinput.isstdin()]
+    print(*meta, end=" ")
+    print(line, end=" ")
